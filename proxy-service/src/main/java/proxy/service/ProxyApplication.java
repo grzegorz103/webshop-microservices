c373f88f5974fbaa803fd1dbadc8e8b54feee9c6
grzegorz103 <grzesiek555_12@outlook.com>
2020-06-10 14:21:03 +0200

    Configure Swagger in proxy-service

--- proxy-service/src/main/java/proxy/service/ProxyApplication.java
+++ proxy-service/src/main/java/proxy/service/ProxyApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableSwagger2
 public class ProxyApplication {
 
     public static void main(String[] args) {
---
+++ proxy-service/src/main/java/proxy/service/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package proxy.service.config;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Primary
+public class SwaggerConfig implements SwaggerResourcesProvider {
+
+    @Override
+    public List<SwaggerResource> get() {
+        return Arrays.asList(
+                swaggerResource("product-service", "/products/api-docs", "2.0")
+        );
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+}
