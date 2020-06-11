92f54e97b08a3dddfb9f9988311ba2611de12bdd
grzegorz103 <grzesiek555_12@outlook.com>
2020-06-11 14:36:51 +0200

    Fix Mapstruct and Lombok work together

--- price-service/src/main/java/price/service/PriceService.java
+++ price-service/src/main/java/price/service/PriceService.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ServiceApplication {
+public class PriceService {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApplication.class, args);
+        SpringApplication.run(PriceService.class, args);
     }
 
 }
