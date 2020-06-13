4b08501888a114567e34523f85946dfb9c4e4887
grzegorz103 <grzesiek555_12@outlook.com>
2020-06-13 13:13:33 +0200

    Add RabbitMq's queue constants

---
+++ common-module/src/main/java/config/ExchangeNames.java
@@ -0,0 +1,9 @@
+package config;
+
+public class ExchangeNames {
+
+    public final static String PRICE_EXCHANGE = "price-exchange";
+
+    public final static String EVENT_EXCHANGE = "event-exchange";
+
+}
