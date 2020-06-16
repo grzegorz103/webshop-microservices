af8631f32876e1ee5f2a3fb70f5915242f2d2a4c
grzegorz103 <grzesiek555_12@outlook.com>
2020-06-16 17:03:34 +0200

    Create order module

---
+++ order-service/src/main/java/order/service/OrderService.java
@@ -0,0 +1,15 @@
+package order.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderService.class, args);
+    }
+
+}
---
+++ order-service/src/main/java/order/service/web/api/OrderController.java
@@ -0,0 +1,11 @@
+package order.service.web.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+}
