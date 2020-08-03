3cad76f1c3854d17697303dc3e1bc6166676ed03
grzegorz103 <grzesiek555_12@outlook.com>
2020-08-03 16:29:07 +0200

    Add angular front end module

---
+++ angular-front/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    NavbarComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
