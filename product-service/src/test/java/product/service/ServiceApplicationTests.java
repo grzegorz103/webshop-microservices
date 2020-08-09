46d7d962f2448b2483395eeb969f816a4c70d090
grzegorz103 <grzesiek555_12@outlook.com>
2020-08-09 19:53:26 +0200

    Add postgres db in docker-compose

--- product-service/src/test/java/product/service/ServiceApplicationTests.java
+++ product-service/src/test/java/product/service/ServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package product.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class ServiceApplicationTests {
 
     @Test
